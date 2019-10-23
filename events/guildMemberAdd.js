b0f17dc383e11b12ba9e1e243a2cdca1f60658b7
Tobias Rauer <prototowb@gmail.com>
2019-10-24 00:00:11 +0200

    welc msg

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,6 +3,7 @@ module.exports = (bot, member) => {
 
         let welcomeChannel = member.guild.channels.find(channel => channel.name === "welcome");
         welcomeChannel.send(`██▓▒░ ${member.displayName} has joined **${member.guild}** ░▒▓██`);
+        welcomeChannel.send("឵឵ ឵឵឵");
         welcomeChannel.send("Welcome <@" + member.id + ">! You are our "+`${member.guild.memberCount}.`+"Member! :tada:");
         
         //let memberRole = member.guild.roles.find(role => role.id == "");
