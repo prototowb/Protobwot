09a963768b58b32272a9f105eeb648de2fac8667
Tobias Rauer <prototowb@gmail.com>
2019-10-24 16:57:35 +0200

    member add msg

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,9 +2,10 @@ module.exports = (bot, member) => {
     bot.on("guildMemberAdd", member => {
 
         let welcomeChannel = member.guild.channels.find(channel => channel.name === "welcome");
-        welcomeChannel.send(`██▓▒░ ${member.displayName} has joined **${member.guild}** ░▒▓██`);
         welcomeChannel.send("឵឵ ឵឵឵");
+        welcomeChannel.send(`██▓▒░ ${member.displayName} has joined **${member.guild}** ░▒▓██`);
         welcomeChannel.send("Welcome <@" + member.id + ">! You are our "+`${member.guild.memberCount}.`+"Member! :tada:");
+        welcomeChannel.send("឵឵ ឵឵឵");
         
         //let memberRole = member.guild.roles.find(role => role.id == "");
         //member.addRole(memberRole);
