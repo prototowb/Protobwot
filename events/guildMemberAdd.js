e4a1533dcd80c94028f20a7b6112bd5a29370710
Tobias Rauer <prototowb@gmail.com>
2019-10-27 16:24:22 +0100

    none

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,10 +2,10 @@ module.exports = (bot, member) => {
     bot.on("guildMemberAdd", member => {
 
         let welcomeChannel = member.guild.channels.find(channel => channel.name === "arrival");
-        welcomeChannel.send("឵឵ ឵឵឵");
-        welcomeChannel.send(`██▓▒░ ${member.displayName} has joined **${member.guild}** ░▒▓██`);
-        welcomeChannel.send("Welcome <@" + member.id + ">! You are our " +`${member.guild.memberCount}.`+"Member! :tada:");
-        welcomeChannel.send("឵឵ ឵឵឵");
+        welcomeChannel.send(" ឵឵឵\n" +
+        `██▓▒░ **${member.displayName}** has joined Proto**Hub** ░▒▓██\n` +
+        "Welcome <@" + member.id + ">! You are our " + `${member.guild.memberCount}.` + "Member! :tada:\n" +
+        "឵឵ ឵឵឵\n");
         
         //let memberRole = member.guild.roles.find(role => role.id == "");
         //member.addRole(memberRole);
