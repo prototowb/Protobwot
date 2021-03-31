85dbc2028881c14bba1f6e310db7336701a9fa58
prototowb <prototowb@gmail.com>
2021-03-31 11:36:39 +0200

    channel names to IDs

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,8 @@ module.exports = bot => {
     console.log("Connected as " + bot.user.tag);
     bot.user.setActivity(`${prefix}help | soon.tm` ,{type: "WATCHING"});
 
-    const roles = bot.channels.cache.find('652120907279761408'); //define permanent role picker channel
-    const welcome = bot.channels.cache.find('811372639133630475'); //define permanent "verify" welcome channel
+    const roles = bot.channels.cache.get('652120907279761408'); //define permanent role picker channel
+    const welcome = bot.channels.cache.get('811372639133630475'); //define permanent "verify" welcome channel
 
     const fetchedChannels = [roles, welcome];
     fetchedChannels.forEach(c => {
