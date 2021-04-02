d4870ccbf83a26a57ef37797219dd549050546fc
prototowb <prototowb@gmail.com>
2021-04-03 00:46:19 +0200

    fixed welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,10 +2,10 @@ module.exports = (bot, member) => {
     bot.once("guildMemberAdd", member => {
 
         const minicatpat = bot.emojis.cache.find(emoji => emoji.name === "minicatpat");
-        let welcomeChannel = member.guild.channels.cache.find(channel => channel.name === "welcome");
+        let welcomeChannel = member.guild.channels.cache.get('651816461983285268');
         welcomeChannel.send("\n" +
-        `░▒▓██  **${member.displayName}** has joined Proto**Code** :tada: \n` +
-        "Welcome <@" + member.id + ">! You are our " + `${member.guild.memberCount}.` + "Member! ${minicatpat} \n" +
+        `░▒▓██  **${member.displayName}** has joined proto**pode** :tada: \n` +
+        "Welcome <@" + member.id + ">! You are our " + `${member.guild.memberCount}.` + `Member! ${minicatpat} \n` +
         "឵឵ ឵឵឵\n");
         
         //let memberRole = member.guild.roles.find(role => role.id == "");
